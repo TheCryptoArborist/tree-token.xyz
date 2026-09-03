c9303b48d91b5174b2e83db4a21245a829ee5719
TheCryptoArborist <ptshopping63@gmail.com>
2026-09-03 15:44:29 -0500

    Configure isolated Netlify preview build

--- tests/netlify-routing-source.test.mjs
+++ tests/netlify-routing-source.test.mjs
@@ -5,6 +5,11 @@ const config = await readFile(new URL('../netlify.toml', import.meta.url), 'utf8
 const packageJson = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf8'));
 const copyScript = await readFile(new URL('../scripts/copy-static-build.mjs', import.meta.url), 'utf8');
 
+assert.match(config, /\[build\][\s\S]*command\s*=\s*"npm run build"/);
+assert.match(config, /\[build\][\s\S]*publish\s*=\s*"dist"/);
+assert.match(config, /\[functions\][\s\S]*directory\s*=\s*"netlify\/functions"/);
+assert.match(config, /\[functions\][\s\S]*node_bundler\s*=\s*"esbuild"/);
+
 assert.equal(
   /from\s*=\s*["']\/dapp["'][\s\S]*?to\s*=\s*["']\/dapp\/["']/.test(config),
   false,
